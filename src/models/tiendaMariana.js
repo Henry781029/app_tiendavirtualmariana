219014ca430e9d20f661b3371e094a45c6f17a65
Henry781029 <61300603+Henry781029@users.noreply.github.com>
2021-09-03 10:09:01 -0500

    conexion bd

---
+++ src/models/tiendaMariana.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const tiendaMariana = new Schema({
+    name :{type: String, require: true},
+    email:{type: String, require: true},
+    country:{type:String, require:true}
+});
+
+const store = mongoose.model('storeMariana',tiendaMariana);
+export default store;
--- src/tiendaApp.js
+++ src/tiendaApp.js
@@ -1,12 +1,29 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import store from './models/tiendaMariana';
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello world!')
+app.get('/', async (req, res) => {
+  try{
+const data= store.find().limit(10);
+res.json(data);
+}
+
+catch(e){
+res.json(e)
+}
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.listen(port,  async () => {
+  try{
+await mongoose.connect('mongodb://localhost:27017/PTvirtualmariana',{
+  useNewUrlParser: true,
+  useUnifieldTopology: true,
+})
+  }
+  catch(e){
+      console.log("crash conection BD");
+  }
+console.log(`Example app listening at http://localhost:${port}`)
 })
